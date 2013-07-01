5b29469bed45c6439239bf8016433f6dcb905a62
Joshua Pekera <joshuapekera@me.com>
2013-06-30 22:28:59 -0500

    Staging with Splash
    setting up a splash site until CMS is in place
    
    Signed-off-by: Joshua Pekera <joshuapekera@me.com>

---
+++ js/libraries.js
@@ -0,0 +1,14 @@
+// Default Placeholder for all Library files to be included via CodeKit
+/*
+$(function(){
+		$pos = $('#topnav').offset().top - 0;
+
+		$(window).on('scroll', function(){
+			if($(window).scrollTop() >= $pos) {
+				$('#topnav').addClass('solid');
+			} else {
+				$('#topnav').removeClass('solid');
+			}
+		});
+	});
+*/;
